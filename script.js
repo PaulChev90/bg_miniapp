8a956f7ad88eb0f9324aabc4ef745e863407d3d4
PaulChev90 <rglokko@me.com>
2025-04-29 14:27:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function setViewportHeight() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setViewportHeight();
+window.addEventListener('resize', setViewportHeight);
+
 const screens = {
   stack: ['main-menu']
 };
